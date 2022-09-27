454ef5e0a13e5c86980f22a7b942486535856bd1
SCHWITZGUEBEL Marc Ext <schwitzguebelmar@rte-france.com>
2022-09-27 11:48:44 +0200

    GRC-186 Add Crac file import to Gridcapa SWE

---
+++ gridcapa-swe-runner-app/src/main/java/com/farao_community/farao/swe/runner/app/services/FileImporter.java
@@ -0,0 +1,50 @@
+package com.farao_community.farao.swe.runner.app.services;
+
+import com.farao_community.farao.data.crac_api.Crac;
+import com.farao_community.farao.data.crac_creation.creator.api.CracCreators;
+import com.farao_community.farao.data.crac_creation.creator.cim.CimCrac;
+import com.farao_community.farao.data.crac_creation.creator.cim.importer.CimCracImporter;
+import com.farao_community.farao.data.crac_io_api.CracImporters;
+import com.farao_community.farao.swe.runner.api.exception.SweInvalidDataException;
+import com.powsybl.iidm.network.Network;
+import org.apache.commons.io.FilenameUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.time.OffsetDateTime;
+
+/**
+ * @author Marc Schwitzguébel {@literal <marc.schwitzguebel at rte-france.com>}
+ */
+@Service
+public class FileImporter {
+
+    private final UrlValidationService urlValidationService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileImporter.class);
+
+    public FileImporter(UrlValidationService urlValidationService) {
+        this.urlValidationService = urlValidationService;
+    }
+
+    public CimCrac importCimCrac(String cracUrl) {
+        InputStream cracInputStream = urlValidationService.openUrlStream(cracUrl);
+        CimCracImporter cimCracImporter = new CimCracImporter();
+        return cimCracImporter.importNativeCrac(cracInputStream);
+    }
+
+    public Crac importCrac(CimCrac cimCrac, OffsetDateTime targetProcessDateTime, Network network) {
+        return CracCreators.createCrac(cimCrac, network, targetProcessDateTime).getCrac();
+    }
+
+    public Crac importCracFromJson(String cracUrl) {
+        try (InputStream cracResultStream = urlValidationService.openUrlStream(cracUrl)) {
+            return CracImporters.importCrac(FilenameUtils.getName(new URL(cracUrl).getPath()), cracResultStream);
+        } catch (IOException e) {
+            throw new SweInvalidDataException(String.format("Cannot import crac from JSON : %s", cracUrl));
+        }
+    }
+}
---
+++ gridcapa-swe-runner-app/src/main/java/com/farao_community/farao/swe/runner/app/services/UrlValidationService.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.farao_community.farao.swe.runner.app.services;
+
+import com.farao_community.farao.swe.runner.api.exception.SweInvalidDataException;
+import com.farao_community.farao.swe.runner.app.configurations.UrlWhitelistConfiguration;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.StringJoiner;
+
+/**
+ * @author Marc Schwitzguébel {@literal <marc.schwitzguebel at rte-france.com>}
+ */
+@Component
+public class UrlValidationService {
+    private final UrlWhitelistConfiguration urlWhitelistConfiguration;
+
+    public UrlValidationService(UrlWhitelistConfiguration urlWhitelistConfiguration) {
+        this.urlWhitelistConfiguration = urlWhitelistConfiguration;
+    }
+
+    public InputStream openUrlStream(String urlString) {
+        if (urlWhitelistConfiguration.getWhitelist().stream().noneMatch(urlString::startsWith)) {
+            StringJoiner sj = new StringJoiner(", ", "Whitelist: ", ".");
+            urlWhitelistConfiguration.getWhitelist().forEach(sj::add);
+            throw new SweInvalidDataException(String.format("URL '%s' is not part of application's whitelisted url's %s", urlString, sj));
+        }
+        try {
+            URL url = new URL(urlString);
+            return url.openStream();
+        } catch (IOException e) {
+            throw new SweInvalidDataException(String.format("Cannot download FileResource file from URL '%s'", urlString), e);
+        }
+    }
+}
