76ac3199c7561b31f62178c9bd187a293450a09d
SCHWITZGUEBEL Marc Ext <schwitzguebelmar@rte-france.com>
2022-09-28 11:04:51 +0200

    GRC-186 - update to remove gridcapa-swe-preprocess-api dependency in pom files

--- gridcapa-swe-runner-app/src/main/java/com/farao_community/farao/swe/runner/app/services/FileExporter.java
+++ gridcapa-swe-runner-app/src/main/java/com/farao_community/farao/swe/runner/app/services/FileExporter.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.nio.file.Paths;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
