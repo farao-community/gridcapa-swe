04cc6e9fe931c2c04562bc7621d76e3dc04d8465
pascolithe <theo.pascoli@rte-france.com>
2022-10-11 09:26:58 +0200

    small changes after remarks

--- gridcapa-swe-runner-app/src/test/java/com/farao_community/farao/swe/runner/app/domain/SweDataTest.java
+++ gridcapa-swe-runner-app/src/test/java/com/farao_community/farao/swe/runner/app/domain/SweDataTest.java
@@ -12,7 +12,6 @@ import com.powsybl.iidm.network.Network;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
