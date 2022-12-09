bf90b743ef78ec44e16b62e0ad5f5b21e38d6ed4
walameni <walha.ameni@gmail.com>
2022-12-09 10:54:01 +0100

    Balancing adjustment for ES PT direction

--- gridcapa-swe-runner-app/src/test/java/com/farao_community/farao/swe/runner/app/dichotomy/shift/SweD2ccShiftDispatcherTest.java
+++ gridcapa-swe-runner-app/src/test/java/com/farao_community/farao/swe/runner/app/dichotomy/shift/SweD2ccShiftDispatcherTest.java
@@ -18,13 +18,42 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class SweD2ccShiftDispatcherTest {
 
+    private final Map<String, Double> intialNetPositions = Map.of("10YES-REE------0", 50., "10YFR-RTE------C", 100., "10YPT-REN------W", 150.);
+
     @Test
-    void dispatch() {
-        Map<String, Double> intialNetPositions = Map.of("10YES-REE------0", 50., "10YFR-RTE------C", 100., "10YPT-REN------W", 150.);
+    void dispatchEsFrTest() {
         SweD2ccShiftDispatcher sweD2ccShiftDispatcher = new SweD2ccShiftDispatcher(DichotomyDirection.ES_FR, intialNetPositions);
         Map<String, Double> shifts = sweD2ccShiftDispatcher.dispatch(1000);
         assertEquals(950., shifts.get("10YES-REE------0"), 0.001);
         assertEquals(-150., shifts.get("10YPT-REN------W"), 0.001);
         assertEquals(-1100., shifts.get("10YFR-RTE------C"), 0.001);
+    }
+
+    @Test
+    void dispatchFrEsTest() {
+        Map<String, Double> intialNetPositions = Map.of("10YES-REE------0", 50., "10YFR-RTE------C", 100., "10YPT-REN------W", 150.);
+        SweD2ccShiftDispatcher sweD2ccShiftDispatcher = new SweD2ccShiftDispatcher(DichotomyDirection.FR_ES, intialNetPositions);
+        Map<String, Double> shifts = sweD2ccShiftDispatcher.dispatch(1000);
+        assertEquals(-1050, shifts.get("10YES-REE------0"), 0.001);
+        assertEquals(-150., shifts.get("10YPT-REN------W"), 0.001);
+        assertEquals(900., shifts.get("10YFR-RTE------C"), 0.001);
+    }
+
+    @Test
+    void dispatchEsPtTest() {
+        SweD2ccShiftDispatcher sweD2ccShiftDispatcher = new SweD2ccShiftDispatcher(DichotomyDirection.ES_PT, intialNetPositions);
+        Map<String, Double> shifts = sweD2ccShiftDispatcher.dispatch(1000);
+        assertEquals(950., shifts.get("10YES-REE------0"), 0.001);
+        assertEquals(-1150., shifts.get("10YPT-REN------W"), 0.001);
+        assertEquals(-100., shifts.get("10YFR-RTE------C"), 0.001);
+    }
+
+    @Test
+    void dispatchPtEsTest() {
+        SweD2ccShiftDispatcher sweD2ccShiftDispatcher = new SweD2ccShiftDispatcher(DichotomyDirection.PT_ES, intialNetPositions);
+        Map<String, Double> shifts = sweD2ccShiftDispatcher.dispatch(1000);
+        assertEquals(-1050., shifts.get("10YES-REE------0"), 0.001);
+        assertEquals(850., shifts.get("10YPT-REN------W"), 0.001);
+        assertEquals(-100., shifts.get("10YFR-RTE------C"), 0.001);
     }
 }
