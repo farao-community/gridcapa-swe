4d41e8848fc3d654713e301c12362b48011fb1fa
SCHWITZGUEBEL Marc Ext <schwitzguebelmar@rte-france.com>
2022-12-09 12:15:10 +0100

    increasing test coverage

--- gridcapa-swe-runner-app/src/main/java/com/farao_community/farao/swe/runner/app/domain/SweDichotomyValidationData.java
+++ gridcapa-swe-runner-app/src/main/java/com/farao_community/farao/swe/runner/app/domain/SweDichotomyValidationData.java
@@ -1,7 +1,17 @@
+/*
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.farao_community.farao.swe.runner.app.domain;
 
 import com.farao_community.farao.monitoring.angle_monitoring.AngleMonitoringResult;
 import com.farao_community.farao.rao_runner.api.resource.RaoResponse;
+
+/**
+ * @author Marc Schwitzguébel {@literal <marc.schwitzguebel at rte-france.com>}
+ */
 
 public class SweDichotomyValidationData {
 
