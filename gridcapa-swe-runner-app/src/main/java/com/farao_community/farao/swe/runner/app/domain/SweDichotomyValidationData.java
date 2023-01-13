b5318f10e2aa0f073675efc659472bb08a7e553b
SCHWITZGUEBEL Marc Ext <schwitzguebelmar@rte-france.com>
2023-01-13 10:59:58 +0100

    update copyright year to 2023 and add second constructor to SweDichotomyValidationData

--- gridcapa-swe-runner-app/src/main/java/com/farao_community/farao/swe/runner/app/domain/SweDichotomyValidationData.java
+++ gridcapa-swe-runner-app/src/main/java/com/farao_community/farao/swe/runner/app/domain/SweDichotomyValidationData.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -21,6 +21,10 @@ public class SweDichotomyValidationData {
     public SweDichotomyValidationData(RaoResponse raoResponse, AngleMonitoringResult angleMonitoringResult) {
         this.raoResponse = raoResponse;
         this.angleMonitoringResult = angleMonitoringResult;
+    }
+
+    public SweDichotomyValidationData(RaoResponse raoResponse) {
+        this(raoResponse, null);
     }
 
     public RaoResponse getRaoResponse() {
--- gridcapa-swe-runner-app/src/test/java/com/farao_community/farao/swe/runner/app/domain/SweDichotomyValidationDataTest.java
+++ gridcapa-swe-runner-app/src/test/java/com/farao_community/farao/swe/runner/app/domain/SweDichotomyValidationDataTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -25,6 +25,14 @@ class SweDichotomyValidationDataTest {
         SweDichotomyValidationData data = new SweDichotomyValidationData(raoResponse, angleMonitoringResult);
         assertEquals(raoResponse, data.getRaoResponse());
         assertEquals(angleMonitoringResult, data.getAngleMonitoringResult());
+    }
+
+    @Test
+    void simpleWithoutAngleMonitoringResultTest() {
+        RaoResponse raoResponse = new RaoResponse("ID", null, null, null, null, null, null);
+        SweDichotomyValidationData data = new SweDichotomyValidationData(raoResponse);
+        assertEquals(raoResponse, data.getRaoResponse());
+        assertNull(data.getAngleMonitoringResult());
     }
 
 }
