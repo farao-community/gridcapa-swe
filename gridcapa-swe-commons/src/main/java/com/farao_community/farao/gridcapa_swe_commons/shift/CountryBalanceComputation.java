348f012562bafaf25b4bc38c2ad4b53b23b27bfd
Theo Pascoli <theo.pascoli@rte-france.com>
2023-10-17 11:17:22 +0200

    set netinterchange values when the basecase loadflow is divergent

--- gridcapa-swe-commons/src/main/java/com/farao_community/farao/gridcapa_swe_commons/shift/CountryBalanceComputation.java
+++ gridcapa-swe-commons/src/main/java/com/farao_community/farao/gridcapa_swe_commons/shift/CountryBalanceComputation.java
@@ -7,9 +7,9 @@
 package com.farao_community.farao.gridcapa_swe_commons.shift;
 
 import com.farao_community.farao.commons.EICode;
-import com.farao_community.farao.gridcapa_swe_commons.exception.SweInternalException;
 import com.powsybl.balances_adjustment.util.CountryArea;
 import com.powsybl.balances_adjustment.util.CountryAreaFactory;
+import com.powsybl.cgmes.extensions.CgmesControlAreas;
 import com.powsybl.computation.local.LocalComputationManager;
 import com.powsybl.iidm.network.Country;
 import com.powsybl.iidm.network.Network;
@@ -30,6 +30,9 @@ import java.util.stream.Stream;
  */
 public final class CountryBalanceComputation {
     private static final Logger LOGGER = LoggerFactory.getLogger(CountryBalanceComputation.class);
+    private static final String CONTROL_AREA_FR = "_7917d422-d956-5595-9489-5ad54aeae656";
+    private static final String CONTROL_AREA_ES = "_cb354e3c-97fe-4fde-f9a6-b3fe750d4a34";
+    private static final String CONTROL_AREA_PT = "_2b6ed858-027e-11e7-afc1-94659cb4e466";
 
     private CountryBalanceComputation() {
          // Should not be instantiated
@@ -38,13 +41,30 @@ public final class CountryBalanceComputation {
     public static Map<String, Double> computeSweCountriesBalances(Network network) {
         LOGGER.info("Computing initial SWE countries balance");
         Map<String, Double> countriesBalances = new HashMap<>();
-        runLoadFlow(network, network.getVariantManager().getWorkingVariantId());
-        Map<String, Double> bordersExchanges = computeSweBordersExchanges(network);
-        countriesBalances.put(toEic("PT"),  -bordersExchanges.get("ES_PT"));
-        countriesBalances.put(toEic("ES"), bordersExchanges.values().stream().reduce(0., Double::sum));
-        countriesBalances.put(toEic("FR"), -bordersExchanges.get("ES_FR"));
+
+        if (runLoadFlow(network, network.getVariantManager().getWorkingVariantId())) {
+            Map<String, Double> bordersExchanges = computeSweBordersExchanges(network);
+            countriesBalances.put(toEic("PT"), -bordersExchanges.get("ES_PT"));
+            countriesBalances.put(toEic("ES"), bordersExchanges.values().stream().reduce(0., Double::sum));
+            countriesBalances.put(toEic("FR"), -bordersExchanges.get("ES_FR"));
+            return countriesBalances;
+        }
+
+        // If the loadflow diverges, we use the netinterchange values instead
+        countriesBalances.put(toEic("PT"), getNetInterchange(network, CONTROL_AREA_PT));
+        countriesBalances.put(toEic("ES"), getNetInterchange(network, CONTROL_AREA_ES));
+        countriesBalances.put(toEic("FR"), getNetInterchange(network, CONTROL_AREA_FR));
 
         return countriesBalances;
+    }
+
+    private static double getNetInterchange(Network network, String id) {
+        return network.getExtension(CgmesControlAreas.class).getCgmesControlArea(id).getNetInterchange();
+    }
+
+    private static boolean runLoadFlow(Network network, String workingStateId) {
+        LoadFlowResult result = LoadFlow.run(network, workingStateId, LocalComputationManager.getDefault(), LoadFlowParameters.load());
+        return result.isOk();
     }
 
     public static Map<String, Double> computeSweBordersExchanges(Network network) {
@@ -54,14 +74,6 @@ public final class CountryBalanceComputation {
         borderExchanges.put("ES_FR", getBorderExchange(Country.ES, Country.FR, countryAreaPerCountry));
         borderExchanges.put("ES_PT", getBorderExchange(Country.ES, Country.PT, countryAreaPerCountry));
         return borderExchanges;
-    }
-
-    private static void runLoadFlow(Network network, String workingStateId) {
-        LoadFlowResult result = LoadFlow.run(network, workingStateId, LocalComputationManager.getDefault(), LoadFlowParameters.load());
-        if (!result.isOk()) {
-            LOGGER.error("Loadflow computation diverged on network '{}'", network.getId());
-            throw new SweInternalException(String.format("Loadflow computation diverged on network %s", network.getId()));
-        }
     }
 
     private static double getBorderExchange(Country fromCountry, Country toCountry, Map<Country, CountryArea> countryAreaPerCountry) {
