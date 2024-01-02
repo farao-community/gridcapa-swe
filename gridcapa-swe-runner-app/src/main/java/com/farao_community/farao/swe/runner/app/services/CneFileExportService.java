79bc4a33a5052855f32239e2d7cc674bcc2bd9c9
SCHWITZGUEBEL Marc Ext <schwitzguebelmar@rte-france.com>
2024-01-02 14:41:45 +0100

    For base case unsecure do not output last secure error file

--- gridcapa-swe-runner-app/src/main/java/com/farao_community/farao/swe/runner/app/services/CneFileExportService.java
+++ gridcapa-swe-runner-app/src/main/java/com/farao_community/farao/swe/runner/app/services/CneFileExportService.java
@@ -76,6 +76,11 @@ public class CneFileExportService {
     }
 
     public String exportCneUrl(SweData sweData, DichotomyResult<SweDichotomyValidationData> dichotomyResult, boolean isHighestValid, DichotomyDirection direction) {
+
+        //if base case unsecure, then is highest valid with limiting cause index evalutation, then we don't want an error file
+        if (!dichotomyResult.hasValidStep() && isHighestValid && dichotomyResult.getLimitingCause() == LimitingCause.INDEX_EVALUATION_OR_MAX_ITERATION) {
+            return null;
+        }
         OffsetDateTime timestamp = sweData.getTimestamp();
         CneExporterParameters cneExporterParameters = getCneExporterParameters(timestamp);
         CimCracCreationContext cracCreationContext = getCimCracCreationContext(sweData, direction);
