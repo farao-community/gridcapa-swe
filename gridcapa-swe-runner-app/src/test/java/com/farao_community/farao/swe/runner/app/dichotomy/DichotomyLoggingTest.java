21fa37aba8ee9d5a4b13f5e69f19ad9533972eac
SCHWITZGUEBEL Marc Ext <schwitzguebelmar@rte-france.com>
2024-01-03 10:48:07 +0100

    Add unit test

--- gridcapa-swe-runner-app/src/test/java/com/farao_community/farao/swe/runner/app/dichotomy/DichotomyLoggingTest.java
+++ gridcapa-swe-runner-app/src/test/java/com/farao_community/farao/swe/runner/app/dichotomy/DichotomyLoggingTest.java
@@ -14,6 +14,8 @@ import org.mockito.Mockito;
 import org.slf4j.Logger;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.util.Collections;
 import java.util.Optional;
 
@@ -30,5 +32,17 @@ class DichotomyLoggingTest {
         assertEquals("FAILURE", ReflectionTestUtils.invokeMethod(businessLogger, "getVoltageCheckResult", DichotomyDirection.ES_FR, Optional.empty()));
         assertEquals("NONE", ReflectionTestUtils.invokeMethod(businessLogger, "getVoltageCheckResult", DichotomyDirection.PT_ES, Optional.empty()));
         assertEquals("SECURE", ReflectionTestUtils.invokeMethod(businessLogger, "getVoltageCheckResult", DichotomyDirection.FR_ES, Optional.of(new VoltageMonitoringResult(Collections.emptyMap(), Collections.emptyMap(), VoltageMonitoringResult.Status.SECURE))));
+    }
+
+    @Test
+    void getTimestampLocalized() {
+        Logger logger = Mockito.mock(Logger.class);
+        ProcessConfiguration processConfiguration = Mockito.mock(ProcessConfiguration.class);
+        Mockito.when(processConfiguration.getZoneId()).thenReturn("Europe/Brussels");
+        DichotomyLogging businessLogger = new DichotomyLogging(logger, processConfiguration);
+        OffsetDateTime timestampSummer = OffsetDateTime.of(2022,6, 18, 23, 29, 1, 0, ZoneOffset.UTC);
+        assertEquals("2022-06-19 01:29", ReflectionTestUtils.invokeMethod(businessLogger, "getTimestampLocalized", timestampSummer));
+        OffsetDateTime timestampWinter = OffsetDateTime.of(2022,11, 18, 22, 27, 2, 0, ZoneOffset.UTC);
+        assertEquals("2022-11-18 23:27", ReflectionTestUtils.invokeMethod(businessLogger, "getTimestampLocalized", timestampWinter));
     }
 }
