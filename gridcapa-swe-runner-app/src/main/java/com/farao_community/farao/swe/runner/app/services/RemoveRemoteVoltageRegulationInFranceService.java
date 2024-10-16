e98368dbf81e1aa9733db2c2a9a2903f7a23726c
Sébastien MURGEY <sebastien.murgey@rte-france.com>
2024-10-16 11:57:00 +0200

    Update after review
    Signed-off-by: Sébastien MURGEY <sebastien.murgey@rte-france.com>

--- gridcapa-swe-runner-app/src/main/java/com/farao_community/farao/swe/runner/app/services/RemoveRemoteVoltageRegulationInFranceService.java
+++ gridcapa-swe-runner-app/src/main/java/com/farao_community/farao/swe/runner/app/services/RemoveRemoteVoltageRegulationInFranceService.java
@@ -6,6 +6,7 @@
  */
 package com.farao_community.farao.swe.runner.app.services;
 
+import com.farao_community.farao.gridcapa_swe_commons.exception.SweInternalException;
 import com.farao_community.farao.swe.runner.app.configurations.DataFixConfiguration;
 import com.powsybl.iidm.network.*;
 import org.slf4j.Logger;
@@ -49,7 +50,10 @@ public class RemoveRemoteVoltageRegulationInFranceService {
     public void resetRemoteVoltageRegulationInFrance(Network network, Map<String, ReplacedVoltageRegulation> generatorsExtension) {
         generatorsExtension.forEach((generatorId, replacedVoltageRegulation) -> {
             Generator generator = network.getGenerator(generatorId);
-            Terminal regulatingTerminal = network.getConnectable(replacedVoltageRegulation.regulatingTerminalConnectableId).getTerminals().stream().filter(terminal -> terminal.getSide() == replacedVoltageRegulation.regulatingTerminalConnectableSide).findAny().get();
+            Terminal regulatingTerminal = network.getConnectable(replacedVoltageRegulation.regulatingTerminalConnectableId).getTerminals().stream()
+                    .filter(terminal -> terminal.getSide() == replacedVoltageRegulation.regulatingTerminalConnectableSide)
+                    .findAny()
+                    .orElseThrow(() -> new SweInternalException("Could not find regulating terminal for resetting remote voltage regulation of generator " + generatorId));
             generator.setRegulatingTerminal(regulatingTerminal);
             generator.setTargetV(replacedVoltageRegulation.value());
         });
