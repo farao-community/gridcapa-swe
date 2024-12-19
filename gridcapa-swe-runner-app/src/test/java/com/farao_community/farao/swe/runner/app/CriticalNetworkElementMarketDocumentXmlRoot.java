fc082fd05ff8dce2504bb63a3e7927ab0fbfac13
Vincent BOCHET <vincent.bochet@rte-france.com>
2024-12-19 09:48:21 +0100

    Fix javax/jakarta migration in tests

--- gridcapa-swe-runner-app/src/test/java/com/farao_community/farao/swe/runner/app/CriticalNetworkElementMarketDocumentXmlRoot.java
+++ gridcapa-swe-runner-app/src/test/java/com/farao_community/farao/swe/runner/app/CriticalNetworkElementMarketDocumentXmlRoot.java
@@ -1,8 +1,7 @@
 package com.farao_community.farao.swe.runner.app;
 
-import com.powsybl.openrao.data.swecneexporter.xsd.CriticalNetworkElementMarketDocument;
-
-import javax.xml.bind.annotation.XmlRootElement;
+import com.powsybl.openrao.data.raoresult.io.cne.swe.xsd.CriticalNetworkElementMarketDocument;
+import jakarta.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "CriticalNetworkElement_MarketDocument", namespace = "urn:iec62325.351:tc57wg16:451-n:cnedocument:2:3")
 public class CriticalNetworkElementMarketDocumentXmlRoot extends CriticalNetworkElementMarketDocument  {
