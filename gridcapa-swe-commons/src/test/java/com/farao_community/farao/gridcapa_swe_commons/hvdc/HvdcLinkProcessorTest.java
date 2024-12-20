6919a7f24117e4246491e466dbb850c79f260c08
KAHYA Amira <amira.kahya@rte-france.com>
2024-12-20 15:08:23 +0100

    small improvement

--- gridcapa-swe-commons/src/test/java/com/farao_community/farao/gridcapa_swe_commons/hvdc/HvdcLinkProcessorTest.java
+++ gridcapa-swe-commons/src/test/java/com/farao_community/farao/gridcapa_swe_commons/hvdc/HvdcLinkProcessorTest.java
@@ -19,7 +19,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.*;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
