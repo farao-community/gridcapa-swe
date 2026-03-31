be820e60b4d2b4203f282d2b45075b53843a44eb
Antoine Limouzin <antoine.limouzin@rte-france.com>
2026-03-31 10:51:38 +0200

    Update gridcapa-swe-commons/src/test/java/com/farao_community/farao/gridcapa_swe_commons/shift/SweNetworkShifterTest.java
    Co-authored-by: vbochetRTE <vincent.bochet@rte-france.com>

--- gridcapa-swe-commons/src/test/java/com/farao_community/farao/gridcapa_swe_commons/shift/SweNetworkShifterTest.java
+++ gridcapa-swe-commons/src/test/java/com/farao_community/farao/gridcapa_swe_commons/shift/SweNetworkShifterTest.java
@@ -241,8 +241,9 @@ class SweNetworkShifterTest {
         Network network = Network.read(networkFileName, getClass().getResourceAsStream(networkFileName));
         Map<String, Double> initialNetPositions = Map.of(ES_EIC, 2317., FR_EIC, -2317., PT_EIC, 0.);
         ShiftDispatcher shiftDispatcher = new SweD2ccShiftDispatcher(FR_ES, initialNetPositions);
-        SweNetworkShifter sweNetworkShifter = new SweNetworkShifter(businessLogger, D2CC,
-                                                                    FR_ES, zonalScalable, shiftDispatcher, 1., 1., initialNetPositions, processConfiguration, LoadFlowParameters.load(), null, true);
+        SweNetworkShifter sweNetworkShifter = new SweNetworkShifter(
+            businessLogger, D2CC, FR_ES, zonalScalable, shiftDispatcher, 1., 1., initialNetPositions, processConfiguration, LoadFlowParameters.load(), null, true
+        );
         Mockito.when(processConfiguration.getShiftMaxIterationNumber()).thenReturn(5);
 
         assertThrows(GlskLimitationException.class, () -> sweNetworkShifter.shiftNetwork(4647, network));
